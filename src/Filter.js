eb44c80660fabced2389b8d21f49fe4b678071d7
Harsh Tomar <harshtomar67tb@gmail.com>
2022-04-20 21:56:02 +0530

    feat: Add  Search & Filter components
    Filter component functionality
    Search component basic structure

--- src/Filter.js
+++ src/Filter.js
@@ -1,10 +1,70 @@
 // styles
+import './Filter.scss'
+
+// icons
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+
+import { useState } from 'react'
+
+const regions = [
+    'africa',
+    'america',
+    'asia',
+    'europe',
+    'oceania',
+]
 
 function Filter() {
-    return(
-        <article>
 
-            filter
+    const [region, setRegion] = useState('Filter by Region')
+
+    const [dropdown, setDropdown] = useState(false)
+
+    // changes selected region
+    const changeRegion = (e) => {
+        const newRegion = e.target.getAttribute('data-val')
+
+        setRegion(newRegion)
+
+        toggleDropdown()
+    }
+
+    // opens/closes dropdown
+    const toggleDropdown = () => {
+        setDropdown(prev => !prev)
+    }
+
+    return(
+        <article className="filter">
+
+            <div 
+                className="filter__btn"
+                onClick={toggleDropdown}
+            >
+
+                <span>
+                    {region}
+                </span>
+
+                <KeyboardArrowDownIcon classNAme="icon" />
+
+            </div>
+
+            { dropdown &&
+                <ul className='filter__data'>
+                
+                    { regions.map(region => (
+                        <li
+                            className='filter__item'
+                            data-val={region}
+                            onClick={changeRegion}
+                        >
+                            {region}
+                        </li>)
+                    )}
+                
+                </ul>
+            }
 
         </article>
     )
--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,19 @@
 // styles
+import './Search.scss'
+
+// icons
+import SearchIcon from '@mui/icons-material/Search'
 
 function Search() {
     return(
         <article className="search">
 
-            search
+            <SearchIcon className="icon" />
+
+            <input
+                type='text'
+                placeholder='Search for a country...'
+            />
 
         </article>
     )
