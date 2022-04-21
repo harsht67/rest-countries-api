28e08f6e8ff4c138eca11753965ab6d7d1853b0a
Harsh Tomar <harshtomar67tb@gmail.com>
2022-04-21 22:00:42 +0530

    feat: Search
    search countries by name

--- src/components/Home/Search.js
+++ src/components/Home/Search.js
@@ -4,15 +4,23 @@ import './Search.scss'
 // icons
 import SearchIcon from '@mui/icons-material/Search'
 
-function Search() {
+function Search(props) {
+
+    const changeHandler = (e) => {
+        const {value} = e.target 
+        props.updateSearch(value)
+    }
+
     return(
         <article className="search">
 
             <SearchIcon className="icon" />
 
             <input
+                value={props.search}
                 type='text'
                 placeholder='Search for a country...'
+                onChange={changeHandler}
             />
 
         </article>
